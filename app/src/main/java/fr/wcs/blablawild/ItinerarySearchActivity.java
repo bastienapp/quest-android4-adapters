301241fe8d2864c2b360fed89467266fc7317ddb
bastienwcs <bastien@wildcodeschool.fr>
2018-02-16 19:40:58 +0100

    Fix

--- app/src/main/java/fr/wcs/blablawild/ItinerarySearchActivity.java
+++ app/src/main/java/fr/wcs/blablawild/ItinerarySearchActivity.java
@@ -1,6 +1,5 @@
 package fr.wcs.blablawild;
 
-import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,13 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
 
 public class ItinerarySearchActivity extends AppCompatActivity {
 
@@ -48,36 +42,5 @@ public class ItinerarySearchActivity extends AppCompatActivity {
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
             }
         });
-
-        final Calendar calendar = Calendar.getInstance();
-        final EditText editDate = findViewById(R.id.edit_date);
-        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                calendar.set(Calendar.YEAR, year);
-                calendar.set(Calendar.MONTH, monthOfYear);
-                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel(editDate, calendar);
-            }
-
-        };
-
-        editDate.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                new DatePickerDialog(ItinerarySearchActivity.this, date, calendar
-                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
-    }
-
-    private void updateLabel(EditText editDate, Calendar calendar) {
-        String myFormat = "MM/dd/yy";
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-
-        editDate.setText(sdf.format(calendar.getTime()));
     }
 }
